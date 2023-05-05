922801932b5b8675c5d9086c6d4af59161597602
bsygo <bjoern.sygo@gmail.com>
2023-05-05 17:03:58 +0200

    Training needs more adaptation
    Currently still plateaus, maybe try failed reward 0 instead of -1,
    reduce contact point rewards, change episode termination.

--- build/lib/bimanual_handover/pc_env.py
+++ build/lib/bimanual_handover/pc_env.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
+from datetime import datetime
 import rospkg
 from geometry_msgs.msg import PoseStamped
 from copy import deepcopy
@@ -16,14 +17,17 @@ class SimpleEnv(gym.Env):
     def __init__(self, fingers, pc, ps = None):
         super().__init__()
         self.action_space = spaces.Box(low = np.array([-1.5, -0.65, -1.25]), high = np.array([1.5, 0.65, 1.25]), dtype = np.float64) # + decision if finished, limits found through manual testing
-        self.observation_space = spaces.Dict({"finger_joints": spaces.Box(-1.134, 1.658, shape = (22,), dtype = np.float64), "finger_contacts": spaces.MultiBinary(5)})# (last_action, finger_joints, finger_contacts) {"last_action": spaces.Box(low = np.array([-1.5, -0.65, -1.25]), high = np.array([1.5, 0.65, 1.25]), dtype = np.float64), 
+        self.observation_space = spaces.Dict({"finger_joints": spaces.Box(-1.134, 1.658, shape = (22,), dtype = np.float64), "finger_contacts": spaces.MultiBinary(5)})# (last_action, finger_joints, finger_contacts) {"last_action": spaces.Box(low = np.array([-1.5, -0.65, -1.25]), high = np.array([1.5, 0.65, 1.25]), dtype = np.float64),
         self.fingers = fingers
         self.pc = pc
         self.ps = ps
-        self.pca_con = sgg.SynGraspGen() 
+        self.pca_con = sgg.SynGraspGen()
         self.last_joints = None
         self.debug_pub = rospy.Publisher('env_debug', PoseStamped)
-        self.log_file = open(rospkg.RosPack().get_path('bimanual_handover') + "/logs/log.txt", 'w')
+        self.time = datetime.now().strftime("%d_%m_%Y_%H_%M")
+        self.log_file = open(rospkg.RosPack().get_path('bimanual_handover') + "/logs/log" + self.time + ".txt", 'w')
+        self.joint_order = self.fingers.get_active_joints()
+        self.reset_timer = 0
 
     def step(self, action):
         reward = 0
@@ -38,9 +42,41 @@ class SimpleEnv(gym.Env):
 #        observation["last_action"] = action
         observation["finger_joints"] = new_joints
         observation["finger_contacts"] = contact_points
+        ff = ['rh_FFJ4', 'rh_FFJ3', 'rh_FFJ2', 'rh_FFJ1']
+        mf = ['rh_MFJ4', 'rh_MFJ3', 'rh_MFJ2', 'rh_MFJ1']
+        rf = ['rh_RFJ4', 'rh_RFJ3', 'rh_RFJ2', 'rh_RFJ1']
+        lf = ['rh_LFJ5', 'rh_LFJ4', 'rh_LFJ3', 'rh_LFJ2', 'rh_LFJ1']
+        th = ['rh_THJ5', 'rh_THJ4', 'rh_THJ3', 'rh_THJ2', 'rh_THJ1']
+        finger_list = [ff, mf, rf, lf, th]
+        if reward != -1:
+            joint_diff = 0
+            for c in range(len(contact_points)):
+                if contact_points[c] == 0:
+                    finger = finger_list[c]
+                    new_joints_finger = [new_joints[self.joint_order.index(joint)] for joint in finger]
+                    last_joints_finger = [self.last_joints[self.joint_order.index(joint)] for joint in finger]
+                    for i in range(len(last_joints_finger)):
+                        if new_joints_finger[i] >= last_joints_finger[i]:
+                            joint_diff += math.dist([new_joints_finger[i]], [last_joints_finger[i]])
+                        else:
+                            joint_diff += - math.dist([new_joints_finger[i]], [last_joints_finger[i]])
+            '''
+            for i in range(len(self.last_joints)):
+                if new_joints[i] >= self.last_joints[i]:
+                    joint_diff += math.dist([new_joints[i]], [self.last_joints[i]])
+                else:
+                    joint_diff += - math.dist([new_joints[i]], [self.last_joints[i]])
+            '''
+            reward = 0.03 * joint_diff
+            reward += np.sum(contact_points)
+            if np.sum(contact_points) == 5:
+                reward += 1
+        '''
         if np.sum(contact_points) > 0 and reward != -1:
             reward = np.sum(contact_points, dtype = np.float64)
-            terminated = True
+            if np.sum(contact_points) == 5:
+                reward += 1
+                terminated = True
         elif reward != -1:
             joint_diff = 0
             for i in range(len(self.last_joints)):
@@ -50,15 +86,22 @@ class SimpleEnv(gym.Env):
                     joint_diff += - math.dist([new_joints[i]], [self.last_joints[i]])
             reward = 0.03 * joint_diff
             # 0.2 * np.sum(contact_points)# reward: 0.03 * distance between new and old joint values during session, 1 or 0 if enough contact points
+        '''
         self.last_joints = observation["finger_joints"]
         truncated = False
         info = {}
         print(action)
         print(reward)
         self.log_file.write("{}, {} \n".format(action, reward))
+        self.reset_timer += 1
+        if self.reset_timer == 20:
+            terminated = True
         return observation, reward, terminated, info
 
     def reset(self):
+        self.log_file.close()
+        self.log_file = open(rospkg.RosPack().get_path('bimanual_handover') + "/logs/log"+ self.time + ".txt", 'a')
+        self.reset_timer = 0
         self.fingers.set_named_target('open')
         self.fingers.go()
         self.pca_con.set_initial_hand_joints()
@@ -100,7 +143,6 @@ class SimpleEnv(gym.Env):
         tip_links = ['rh_ff_biotac_link', 'rh_mf_biotac_link', 'rh_rf_biotac_link', 'rh_lf_biotac_link', 'rh_th_biotac_link']
         tip_points = [[self.fingers.get_current_pose(link).pose.position.x, self.fingers.get_current_pose(link).pose.position.y, self.fingers.get_current_pose(link).pose.position.z] for link in tip_links]
         contact_points = [0, 0, 0, 0, 0]
-        print(r)
         for x in range(len(tip_points)):
             if np.linalg.norm(np.cross(tip_points[x] - p1, tip_points[x] - p2))/np.linalg.norm(p2 - p1) <= r:
                 contact_points[x] = 1
--- scripts/train_ppo.py
+++ scripts/train_ppo.py
@@ -14,21 +14,13 @@ from stable_baselines3.common.logger import configure
 from sensor_msgs.msg import PointCloud2
 from tqdm.auto import tqdm
 from stable_baselines3.common.callbacks import CheckpointCallback
+import re
 
 
 def end_moveit():
     roscpp_shutdown()
 
 def main(argv):
-    if len(argv <= 3):
-        test = False
-    else:
-        args_pattern = re.compile(r"""((?:--test)\s(?P<TEST>True(\s(?:--model)\s(?P<MODEL>.*?)\s)?|False)\s)""")
-        if match_object := args_pattern.match(argv[3:]):
-            args = {k: v for k, v in match_object.groupdict().items() if v is not None}
-        test = True
-        print(args)
-    return
     rospy.init_node('ppo_trainer')
     roscpp_initialize('')
     rospy.on_shutdown(end_moveit)
@@ -38,23 +30,52 @@ def main(argv):
     ps = PlanningSceneInterface()
     fingers.set_max_velocity_scaling_factor(1.0)
     fingers.set_max_acceleration_scaling_factor(1.0)
+
+    args_pattern = re.compile(r"(((?:--check)\s(?P<CHECK>True|False))?\s?((?:--test)\s(?P<TEST>True|False))?\s?((?:--model)\s(?P<MODEL>.*))?)")
+    args = {}
+    if len(argv) > 1:
+        argv = " ".join(argv[1:])
+    else:
+        argv = ""
+    if match_object := args_pattern.match(argv):
+        args = {k: v for k, v in match_object.groupdict().items() if v is not None}
+    if 'CHECK' in args:
+        check = args ['CHECK']
+    else:
+        check = False
+        rospy.loginfo('No check parameter specified, defaulting to False.')
+    if 'TEST' in args:
+        test = args['TEST']
+    else:
+        test = False
+        rospy.loginfo('No test parameter specified, defaulting to False.')
+    if 'MODEL' in args:
+        model_path = args['MODEL']
+    else:
+        model_path = path + "/models/ppo_model"
+        rospy.loginfo('No model parameter specified, defaulting to standard model.')
+
     rospy.loginfo('Waiting for pc.')
     pc = rospy.wait_for_message('pc/pc_filtered', PointCloud2, 20)
     rospy.loginfo('Setting up env.')
     env = pc_env.SimpleEnv(fingers, pc, ps)
+
     if not test:
-    #    check_env(env)
-    #    rospy.loginfo('Env check completed.')
+        if check:
+            check_env(env)
+            rospy.loginfo('Env check completed.')
         date = datetime.now()
         str_date = date.strftime("%d_%m_%Y_%H_%M")
         checkpoint_callback = CheckpointCallback(save_freq = 500, save_path = path + "/models/", name_prefix = "ppo_checkpoint_" + str_date, save_replay_buffer = True, save_vecnormalize = True)
         model = PPO("MultiInputPolicy", env, n_steps = 20, batch_size = 5, n_epochs = 100, verbose = 1, tensorboard_log=path + "/logs/tensorboard")
-        rospy.loginfo('Starting learning.')
+        rospy.loginfo('Start learning.')
         model.learn(total_timesteps=2000, progress_bar = True, callback = checkpoint_callback)
-
+        rospy.loginfo('Learning complete.')
         model.save(path + "/models/ppo_model_" + str_date)
     else:
-        model = PPO.load("../ppo_model")
+        model = PPO.load(model_path)
+
+    rospy.loginfo('Execute sample motion.')
     obs = env.reset()
     action, _states = model.predict(obs)
     obs, reward, terminated, info = env.step(action)

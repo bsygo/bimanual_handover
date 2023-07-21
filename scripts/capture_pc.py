6e63c6a80a6f21c8bd370d64cf5c351ff95ed8ef
bsygo <bjoern.sygo@gmail.com>
2023-07-21 14:55:47 +0200

    Updated pc infrastructure
    Changed namespace for all pc related topics to handover/pc. Also changed
    the capture_pc script to only publish the newest pc when requested
    instead of constantly publishing the latest requested one.

--- scripts/capture_pc.py
+++ scripts/capture_pc.py
@@ -4,30 +4,26 @@ import rospy
 from sensor_msgs.msg import PointCloud2
 from std_msgs.msg import Bool
 
-global publish, pc
+global pub, pc
 
-def change_publish(value):
-    global publish, pc
-    if not publish and value.data:
-        publish = value.data
-        pc = rospy.wait_for_message('/azure_kinect/points2', PointCloud2)
-
-def republish_pc():
+def save_pc(new_pc):
     global pc
-    pub = rospy.Publisher('/cloud_pcd', PointCloud2, queue_size=5)
-    pc = rospy.wait_for_message('/azure_kinect/points2', PointCloud2)
-    while not rospy.is_shutdown():
-        if publish:
-            pc.header.stamp = rospy.Time(0)
-            pub.publish(pc)
-        rospy.sleep(1)
+    pc = new_pc
+
+def publish_pc(publish):
+    global pub, pc
+    if publish.data:
+        pub.publish(pc)
 
 def main():
-    global publish, pc
+    global pub, pc
     rospy.init_node('capture_pc')
     publish = False
-    rospy.Subscriber('change_publish', Bool, change_publish, queue_size = 1)
-    republish_pc()
+    pc = rospy.wait_for_message('/azure_kinect/points2', PointCloud2)
+    rospy.Subscriber('/azure_kinect/points2', PointCloud2, save_pc, queue_size = 1)
+    rospy.Subscriber('handover/pc/publish_pc', Bool, publish_pc, queue_size = 1)
+    pub = rospy.Publisher('/cloud_pcd', PointCloud2, queue_size=5)
+    rospy.spin()
 
 if __name__ == "__main__":
     main()

a07f86c22023a1cf5eee52af74f3c933728b7a79
bsygo <bjoern.sygo@gmail.com>
2023-07-26 14:17:50 +0200

    Added train launch file
    Adapted handover controller to take argument to switch between train
    launch and full launch.

--- src/bimanual_handover/handover_controller.py
+++ src/bimanual_handover/handover_controller.py
@@ -2,10 +2,11 @@
 
 import rospy
 from bimanual_handover.srv import InitGripper, ProcessPC, MoveHandover, GraspExec, FinishHandoverSrv
+import sys
 
 class HandoverCommander():
 
-    def __init__(self):
+    def __init__(self, handover_type):
         rospy.init_node('handover_commander')
         rospy.loginfo('Handover commander started.')
         rospy.wait_for_service('handover/init_gripper_srv')
@@ -22,6 +23,17 @@ class HandoverCommander():
         rospy.loginfo('grasp_exec_srv initialized.')
         self.finish_handover_srv = rospy.ServiceProxy('handover/finish_handover_srv', FinishHandoverSrv)
         rospy.loginfo('finish_handover_srv initialized.')
+        slef.launch_hadover(handover_type)
+
+    def launch_hadover(self, handover_type):
+        if handover_type == "full":
+            rospy.loginfo("Launchng full handover pipeline.")
+            self.full_pipeline()
+        elif handover_type == "train":
+            rospy.loginfo("Launching handover training setup.")
+            self.train_setup()
+        else:
+            rospy.loginfo("Unknown handover command {}.".format(handover_type))
 
     def full_pipeline(self):
         rospy.loginfo('Sending service request to init_gripper_srv.')
@@ -38,7 +50,20 @@ class HandoverCommander():
         self.finish_handover_srv('placeholder')
         rospy.loginfo('Handover finished.')
 
+    def train_setup(self):
+        rospy.loginfo('Sending service request to init_gripper_srv.')
+        if not self.init_gripper_srv('fixed'):
+            rospy.logerr('Moving gripper to inital pose failed.')
+            return
+        rospy.loginfo('Sending service request to process_pc_srv.')
+        self.process_pc_srv(True)
+        rospy.loginfo('Sending service request to move_handover_srv.')
+        self.move_handover_srv('fixed')
+        rospy.loginfo('Training setup finished.')
+        
 
 if __name__ == "__main__":
-    handover_commander = HandoverCommander()
-    handover_commander.full_pipeline()
+    if len(sys.argv) < 2:
+        rospy.loginfo('Missing parameter for handover_commander.')
+    else:    
+        handover_commander = HandoverCommander(sys.argv[1])

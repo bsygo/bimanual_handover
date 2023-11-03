14f1496f933bb8ebf4332980210071eb838a9d4b
pr2 <pr2@tams>
2023-11-03 14:42:20 +0100

    Additional adjustments to ccm model_mover change.

--- scripts/grasp_stub.py
+++ scripts/grasp_stub.py
@@ -2,42 +2,22 @@
 
 import rospy
 import sys
-from bimanual_handover_msgs.srv import GraspExec, CCM, GraspTesterSrv, ModelMoverSrv
+from bimanual_handover_msgs.srv import GraspExec, GraspTesterSrv, HandCloserSrv
 
-global ccm_srv, grasp_tester_srv, model_mover_srv
+global grasp_tester_srv, hand_closer_srv
 
 def grasp_exec(req):
-    if req.mode == "ccm":
-        close_grasp = ccm_srv(req.mode).finished
-        rospy.loginfo("Closing grasp result: {}".format(close_grasp))
-        if close_grasp:
-            return grasp_tester_srv('placeholder').success
-        return False
-    elif req.mode == "pca":
-        close_grasp = model_mover_srv(req.mode).finished
-        rospy.loginfo("PCA grasp result: {}".format(close_grasp))
-        if close_grasp:
-            return grasp_tester_srv('placeholder').success
-        return False
-    else:
-        rospy.logerr("Unknown grasping mode {}.".format(req.mode))
-        return False
+    close_grasp = hand_closer_srv(req.mode).finished
+    rospy.loginfo("Grasp result: {}".format(close_grasp))
+    if close_grasp:
+        return grasp_tester_srv('y').success
+    return False
 
 def main():
-    global ccm_srv, grasp_tester_srv, model_mover_srv
+    global grasp_tester_srv, hand_closer_srv
     rospy.init_node('grasp_stub')
-    if len(sys.argv) < 2:
-        rospy.logerr("Missing argument for grasp_stub.")
-        return
-    elif sys.argv[1] == 'ccm':
-        rospy.wait_for_service('ccm')
-        ccm_srv = rospy.ServiceProxy('ccm', CCM)
-    elif sys.argv[1] == 'pca':
-        rospy.wait_for_service('model_mover_srv')
-        model_mover_srv = rospy.ServiceProxy('model_mover_srv', ModelMoverSrv)
-    else:
-        rospy.logerr("Unknown argument {} for grasp_stub.".format(sys.argv[1]))
-        return
+    rospy.wait_for_service('hand_closer_srv')
+    model_mover_srv = rospy.ServiceProxy('hand_closer_srv', HandCloserSrv)
     rospy.wait_for_service('grasp_tester')
     grasp_tester_srv = rospy.ServiceProxy('grasp_tester', GraspTesterSrv)
     rospy.Service('grasp_exec_srv', GraspExec, grasp_exec)

3ce73b1ec2e5eff28b716611807a670bbcb6ac74
pr2 <pr2@tams>
2023-11-03 14:39:34 +0100

    Combined ccm and model_mover.
    Now both provide the same general service, hand_closer_srv, and are different classes in the file hand_closer.py.

--- src/bimanual_handover/hand_closer.py
+++ src/bimanual_handover/hand_closer.py
@@ -2,13 +2,19 @@
 
 import rospy
 import rospkg
+import math
+import sys
+import numpy as np
+from stable_baselines3 import PPO, SAC
+import bimanual_handover.syn_grasp_gen as sgg
 from rosbag import Bag
 from datetime import datetime
 from std_msgs.msg import Float64
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from sr_robot_msgs.msg import BiotacAll
-from bimanual_handover_msgs.srv import CCM
+from bimanual_handover_msgs.srv import HandCloserSrv
+from moveit_commander import MoveGroupCommander
 import actionlib
 from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
 from copy import deepcopy
@@ -43,7 +49,7 @@ class CloseContactMover():
             name = "closing_attempt_" + current_date + ".bag"
             self.data_bag = Bag(pkg_path + "/data/" + name, 'w')
         self.joint_client.wait_for_server()
-        rospy.Service('ccm', CCM, self.move_until_contacts)
+        rospy.Service('hand_closer_src', HandCloserSrv, self.move_until_contacts)
         rospy.spin()
 
     def wait_for_initial_values(self):
@@ -157,5 +163,107 @@ class CloseContactMover():
         print(contacts)
         return False
 
+class ModelMover():
+
+    def __init__(self, model_name = "sac_checkpoint_23_10_2023_15_47_12900_steps.zip"):
+        rospy.init_node('model_mover')
+        rospack = rospkg.RosPack()
+        pkg_path = rospack.get_path('bimanual_handover')
+        model_path = pkg_path + "/models/" + "checkpoints/" + model_name
+        self.model = SAC.load(model_path)
+        self.model.load_replay_buffer(pkg_path + "/models/checkpoints/sac_checkpoint_23_10_2023_15_47_replay_buffer_12900_steps")
+        self.fingers = MoveGroupCommander('right_fingers', ns="/")
+        self.fingers.set_max_velocity_scaling_factor(1.0)
+        self.fingers.set_max_acceleration_scaling_factor(1.0)
+        self.initial_tactile = None
+        self.current_tactile = None
+        self.current_pca = None
+        self.tactile_sub = rospy.Subscriber('/hand/rh/tactile', BiotacAll, self.tactile_callback)
+        self.joints = ['rh_WRJ2', 'rh_WRJ1', 'rh_FFJ4', 'rh_FFJ3', 'rh_FFJ2', 'rh_FFJ1', 'rh_LFJ5', 'rh_LFJ4', 'rh_LFJ3', 'rh_LFJ2', 'rh_LFJ1', 'rh_MFJ4', 'rh_MFJ3', 'rh_MFJ2', 'rh_MFJ1', 'rh_RFJ4', 'rh_RFJ3', 'rh_RFJ2', 'rh_RFJ1', 'rh_THJ5', 'rh_THJ4', 'rh_THJ3', 'rh_THJ2', 'rh_THJ1']
+        self.pca_con = sgg.SynGraspGen()
+        self.joint_state_sub = rospy.Subscriber('/joint_states', JointState, callback = self.joint_callback, queue_size = 10)
+        #self.joint_state_sub = rospy.Subscriber('/hand/joint_states', JointState, callback = self.joint_callback, queue_size = 10)
+        rospy.Service('hand_closer_srv', HandCloserSrv, self.close_hand)
+        rospy.spin()
+
+    def tactile_callback(self, tactile):
+        self.current_tactile = [x.pdc for x in tactile.tactiles]
+
+    def joint_callback(self, joint_state):
+        if not self.joints[0] in joint_state.name:
+            return
+        indices = [joint_state.name.index(joint_name) for joint_name in self.joints]
+        current_joint_values = [joint_state.position[x] for x in indices]
+        current_joint_values = np.array(current_joint_values, dtype = np.float32)
+        self.current_pca = self.pca_con.get_pca_config(current_joint_values)[0][:3]
+
+    def get_next_action(self):
+        current_biotac = deepcopy(self.current_tactile)
+        current_observation = [current_biotac[x] - self.initial_tactile[x] for x in range(len(current_biotac))]
+        current_pca = self.pca_con.get_pca_config()[0][:3]
+        for pca in current_pca:
+            current_observation.append(pca)
+        observation = np.array(current_observation, dtype = np.float32)
+        action, _states = self.model.predict(observation, deterministic = True)
+        return action
+
+    def exec_action(self, action):
+        # Stop moving if enough contacts have been made
+        current_biotac = deepcopy(self.current_tactile)
+        current_biotac_diff = [current_biotac[x] - self.initial_tactile[x] for x in range(len(current_biotac))]
+        contacts = [True if diff >=20 else False for diff in current_biotac_diff]
+        # Reduce normalized actions
+        action[0] = (action[0] - 1)/2
+        action[1] = (action[1] - 1)/2
+        # Get new config
+        result = self.pca_con.gen_joint_config(action[:3], normalize = True)
+        # Remove wrist joints
+        del result['rh_WRJ1']
+        del result['rh_WRJ2']
+
+        # Remove fingers with contact (optional, testing) -> Mabe switch to not close anymore instead of no movement at all
+        del_keys = []
+        if contacts[0]:
+            for key in result.keys():
+                if 'rh_FFJ' in key:
+                    del_keys.append(key)
+        if contacts[1]:
+            for key in result.keys():
+                if 'rh_MFJ' in key:
+                    del_keys.append(key)
+        if contacts[2]:
+            for key in result.keys():
+                if 'rh_RFJ' in key:
+                    del_keys.append(key)
+        if contacts[3]:
+            for key in result.keys():
+                if 'rh_LFJ' in key:
+                    del_keys.append(key)
+        if contacts[4]:
+            for key in result.keys():
+                if 'rh_THJ' in key:
+                    del_keys.append(key)
+        for key in del_keys:
+            del result[key]
+
+        self.fingers.set_joint_value_target(result)
+        self.fingers.go()
+
+    def close_hand(self, req):
+        self.initial_tactile = deepcopy(self.current_tactile)
+        for i in range(20):
+            action = self.get_next_action()
+            self.exec_action(action)
+        return True
+
+def init_mover(mode):
+    if mode == "ccm":
+        CloseContactMover()
+    elif mode == "pca":
+        ModelMover()
+
 if __name__ == '__main__':
-    ccm = CloseContactMover()
+    if len(sys.argv) < 2:
+        rospy.logerr("Missing parameters to call hand_closer. Exiting.")
+    else:
+        init_mover(sys.argv[1]) 

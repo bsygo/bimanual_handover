c0328c69e276d97d8c2545bfe7d218418f8bbe8f
pr2 <pr2@tams>
2023-11-17 15:16:01 +0100

    Move relative to gripper instead of y-axis
    Previously, the detach movement of the gripper was relative to the global y-axis, not relative to the gripper pose. Now, the movement is relative to the gripper. The axis might need to be changed now.

--- src/bimanual_handover/finish_handover.py
+++ src/bimanual_handover/finish_handover.py
@@ -3,20 +3,23 @@
 import rospy
 from moveit_commander import MoveGroupCommander, roscpp_initialize, roscpp_shutdown, PlanningSceneInterface
 from bimanual_handover_msgs.srv import FinishHandoverSrv
-from geometry_msgs.msg import PoseStamped, Quaternion
+from geometry_msgs.msg import PoseStamped, Quaternion, Pose
 from tf.transformations import quaternion_multiply, quaternion_from_euler
+from tf2_ros import TransformListener, Buffer
+from tf2_geometry_msgs import do_transform_pose
 
 class FinishHandover():
 
     def __init__(self):
         rospy.init_node('finish_handover')
-        roscpp_initialize('')
         rospy.on_shutdown(self.shutdown)
         self.gripper = MoveGroupCommander('left_gripper', ns = "/")
         self.left_arm = MoveGroupCommander('left_arm', ns = "/")
         self.right_arm = MoveGroupCommander('right_arm_pr2', ns = "/")
         self.right_hand = MoveGroupCommander('right_arm', ns = "/")
         self.psi = PlanningSceneInterface(ns = "/")
+        self.tf_buffer = Buffer()
+        TransformListener(tf_buffer)
         self.debug_pub = rospy.Publisher('debug/finish_handover', PoseStamped, latch = True, queue_size = 1)
         rospy.Service('finish_handover_srv', FinishHandoverSrv, self.finish_handover)
         rospy.spin()
@@ -29,8 +32,12 @@ class FinishHandover():
         #self.psi.remove_world_object('can')
         self.gripper.set_named_target('open')
         self.gripper.go()
-        gripper_pose = self.left_arm.get_current_pose()
-        gripper_pose.pose.position.y += 0.05
+        gripper_pose = PoseStamped()
+        gripper_pose.header.frame_id = "l_gripper_tool_frame"
+        gripper_pose.pose.position.y += -0.05
+        gripper_pose.pose.orientation.w = 1
+        gripper_base_transform = self.tf_buffer.lookup_transform("base_footprint", "l_gripper_tool_frame", rospy.Time(0))
+        gripper_pose = do_transform_pose(gripper_pose, gripper_base_transform)
         plan, fraction = self.left_arm.compute_cartesian_path([gripper_pose.pose], 0.01, 0.0)
         if fraction < 1.0:
             rospy.loginfo('Only {} of the path to remove the gripper from the object found.'.format(fraction))

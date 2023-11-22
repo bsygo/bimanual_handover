b125523f843e32b0593bce9b74f164146ec98560
pr2 <pr2@tams>
2023-11-22 17:16:41 +0100

    Call handover_controller with object_type

--- scripts/launch_handover.py
+++ scripts/launch_handover.py
@@ -7,14 +7,16 @@ import sys
 def main():
     rospy.wait_for_service('handover_controller_srv')
     srv = rospy.ServiceProxy('handover_controller_srv', HandoverControllerSrv)
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 4:
         rospy.loginfo('Missing parameters to call handover_controller. Calling with default parameters.')
         grasp_type = 'ccm'
         handover_type = 'full'
+        object_type = 'can'
     else:
         grasp_type = sys.argv[1]
         handover_type = sys.argv[2]
-    result = srv(grasp_type, handover_type)
+        object_type = sys.argv[3]
+    result = srv(grasp_type, handover_type, object_type)
     rospy.loginfo('Handover controller finished with {}.'.format(result.success))
 
 if __name__ == "__main__":

d6af31b3c939f222b51a03e49cce3e250cda583c
pr2 <pr2@tams>
2024-01-24 17:00:59 +0100

    Switch to fixed pose instead of relative

--- src/bimanual_handover/finish_handover.py
+++ src/bimanual_handover/finish_handover.py
@@ -11,17 +11,27 @@ from tf2_geometry_msgs import do_transform_pose
 class FinishHandover():
 
     def __init__(self):
+        # Initialize
         rospy.init_node('finish_handover')
         roscpp_initialize('')
         rospy.on_shutdown(self.shutdown)
+
+        # Setup move_groups
         self.gripper = MoveGroupCommander('left_gripper', ns = "/")
         self.left_arm = MoveGroupCommander('left_arm', ns = "/")
         self.right_arm = MoveGroupCommander('right_arm_pr2', ns = "/")
         self.right_hand = MoveGroupCommander('right_arm', ns = "/")
-        self.psi = PlanningSceneInterface(ns = "/")
+
+        # Setup tf buffer
         self.tf_buffer = Buffer()
         TransformListener(self.tf_buffer)
-        self.debug_pub = rospy.Publisher('debug/finish_handover', PoseStamped, latch = True, queue_size = 1)
+
+        # Debug
+        self.debug = rospy.get_param("finish_handover/debug")
+        if self.debug:
+            self.debug_pub = rospy.Publisher('debug/finish_handover', PoseStamped, latch = True, queue_size = 1)
+
+        # Start service
         rospy.Service('finish_handover_srv', FinishHandoverSrv, self.finish_handover)
         rospy.spin()
 
@@ -29,10 +39,11 @@ class FinishHandover():
         roscpp_shutdown()
 
     def finish_handover(self, req):
-        # Currently not used, maybe later for collision checking/visualization
-        #self.psi.remove_world_object('can')
+        # Open the gripper
         self.gripper.set_named_target('open')
         self.gripper.go()
+
+        # Retract the gripper
         gripper_pose = PoseStamped()
         gripper_pose.header.frame_id = "l_gripper_tool_frame"
         gripper_pose.pose.position.x += -0.1
@@ -43,16 +54,23 @@ class FinishHandover():
         if fraction < 0.8:
             rospy.loginfo('Only {} of the path to remove the gripper from the object found.'.format(fraction))
         self.left_arm.execute(plan)
+
+        # Move left arm to side
         self.left_arm.set_named_target('left_arm_to_side')
         self.left_arm.go()
-        #self.right_arm.set_named_target('right_arm_to_side')
-        final_pose = self.right_hand.get_current_pose()
-        final_pose.pose.position.y += -0.5
-        final_pose.pose.position.x += 0.15
-        orientation_change = quaternion_from_euler(0, 0, -1.5708)
-        old_orientation = [final_pose.pose.orientation.x, final_pose.pose.orientation.y, final_pose.pose.orientation.z, final_pose.pose.orientation.w]
-        final_pose.pose.orientation = Quaternion(*quaternion_multiply(orientation_change, old_orientation).tolist())
-        self.debug_pub.publish(final_pose)
+
+        # Set final pose for right arm
+        final_pose = PoseStamped()
+        final_pose.header.frame_id = "base_footprint"
+        final_pose.pose.position.x = 0.255
+        final_pose.pose.position.y = -0.662
+        final_pose.pose.position.z = 0.675
+        final_pose.pose.orientation = Quaternion(*quaternion_from_euler(1.5707, 1.5708, 0))
+
+        if self.debug:
+            self.debug_pub.publish(final_pose)
+
+        # Move right arm to final pose
         self.right_hand.set_pose_target(final_pose)
         self.right_hand.go()
         return True
